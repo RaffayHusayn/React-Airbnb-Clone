b4b1abe4d548fa42832b2f1315b472f76d68b71d
Raffay Syed <raffayhusayn@gmail.com>
2022-06-27 23:33:14 -0400

    Add css styles to the card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Card from './components/Card';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Hero />
+
+      {/* <Hero /> */}
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import katieImage from '../images/katie.jpeg';
+import starImage from '../images/star.png';
+import '../styles/Card.css';
+
+function Card(){
+    return(
+        <div className="card">
+            <img src={katieImage} alt="card-person" className="card-image" />
+            <div className="card-container">
+                <div className="card-rating">
+                    <img src={starImage} alt="star-rating" className="card-star" />
+                    <p className="card-rating-text">5.0 <span className='gray'>(6) • USA</span> </p>
+                </div>
+                <p className="card-description">Life lesson with Katie</p>
+                <p className="card-price"><b>From $135</b>/ person</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
