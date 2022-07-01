5e3667121fe96b3ac6e7a486f3ea56ad8a946739
Raffay Syed <raffayhusayn@gmail.com>
2022-06-30 21:22:54 -0400

    Add code to add a "Sold Out" tag on the card component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import Navbar from './components/Navbar';
-// import Hero from './components/Hero';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 import cards from "./Data/CardData.js";
-import CardSlider from './components/CardSlider';
-import './App.css';
-
-
-
+import CardSlider from "./components/CardSlider";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
 
-      {/* <Hero /> */}
-      {/* <Card 
-      image={katieImage}
-      rating={{stars: 5.0, totalratings: 6, location: "USA"}}
-      desc="life lesson with raffay"
-      price="400"
-      /> */}
+      <Hero />
       <CardSlider cards={cards} />
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,28 @@
-import starImage from '../images/star.png';
-import '../styles/Card.css';
+import starImage from "../images/star.png";
+import "../styles/Card.css";
 
-function Card(props){
-    return(
-        <div className="card">
-            <img src={props.image} alt="card-person" className="card-image" />
-            <div className="card-container">
-                <div className="card-rating">
-                    <img src={starImage} alt="star-rating" className="card-star" />
-                    <p className="card-rating-text">{props.rating.stars}<span className='gray'>({props.rating.totalratings})•{props.rating.location}</span> </p>
-                </div>
-                <p className="card-description">{props.desc}</p>
-                <p className="card-price"><b>${props.price}</b>/ person</p>
-            </div>
+function Card(props) {
+  return (
+    <div className="card">
+      <img src={props.image} alt="card-person" className="card-image" />
+      <div className="card-container">
+        <div className="card-tag">SOLD OUT</div>
+        <div className="card-rating">
+          <img src={starImage} alt="star-rating" className="card-star" />
+          <p className="card-rating-text">
+            {props.rating.stars}
+            <span className="gray">
+              ({props.rating.totalratings})•{props.rating.location}
+            </span>{" "}
+          </p>
         </div>
-    )
+        <p className="card-description">{props.desc}</p>
+        <p className="card-price">
+          <b>${props.price}</b>/ person
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
